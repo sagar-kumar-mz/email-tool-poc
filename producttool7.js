c1dac7fc4a1b6e1d547923fce7f430070781cf11
sagar-kumar-mz <98985336+sagar-kumar-mz@users.noreply.github.com>
2022-04-07 16:40:21 +0530

    Update producttool7.js

--- producttool7.js
+++ producttool7.js
@@ -1,5 +1,5 @@
 const editorTemplate = '<button id="addProduct" class="button">Add Speaker</button>';
-console.log('Dada :>', products, item, data, values)
+console.log('Dada :>',  item, data, values)
 const productItemsTemplate = _.template(`
 <% _.forEach(products, function(item) { %>
   <div class="product-item" id="product-item" data-uuid='<%= item.id %>' data-title="<%= item.name %>" data-designation="<%= item.designation %>" data-image="<%= item.profile_img %>" data-company="<%= item.company %>" >
