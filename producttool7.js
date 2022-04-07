f9314a9a9f4025c77fd8c8417d2993ea49e6221a
sagar-kumar-mz <98985336+sagar-kumar-mz@users.noreply.github.com>
2022-04-07 18:22:08 +0530

    Update producttool7.js

--- producttool7.js
+++ producttool7.js
@@ -42,7 +42,7 @@ console.log('values', values)
       values?.speakerImage?.url ? values?.speakerImage?.url : 'https://cms-assets.tutsplus.com/cdn-cgi/image/width=630/uploads/users/988/posts/31255/image/What-is-public-speaking%20(1).jpg'
     }" style="width: 100%; object-fit: contain; border-top-left-radius: 4px; border-top-right-radius: 4px;" />
     <div class="product-card-body" style="padding: 0 16px 16px;text-align: left;">
-      <h3 style="margin: 12px 0; color: ${values.speakerTitleColor};">${values.speakerTitle ? values.speakerTitle : 'Speaker Name'}</h3>
+      <h3 style="margin: 12px 0; color: ${values.speakerTitleColor === "#000000" ? '#ed1515' : values.speakerTitleColor };">${values.speakerTitle ? values.speakerTitle : 'Speaker Name'}</h3>
       <div class="description" style="color: ${values.speakerDesignationCompanyColor};">
       ${values.speakerEmail ? values.speakerEmail: 'Designation'}, ${values.speakerAbout ? values.speakerAbout : 'Company'}</div>
       <p>toolTemplate</p>
@@ -63,9 +63,7 @@ console.log('values', values)
   }" style="width: 100%; object-fit: contain; border-top-left-radius: 4px; border-top-right-radius: 4px;" /></td></tr>
         <tr><td width="100%"><h3 id="${
           values?.speakerLibrary?.selected?.id
-        }-speakerTitle" style="text-align: left;margin: 8px 0 12px 0; padding: 0 16px; color: ${
-    values.speakerTitleColor
-  };">${values.speakerTitle ? values.speakerTitle : 'Speaker Name'}</h3></td></tr>
+        }-speakerTitle" style="text-align: left;margin: 8px 0 12px 0; padding: 0 16px; color: ${values.speakerTitleColor === "#000000" ? '#ed1515' : values.speakerTitleColor };">${values.speakerTitle ? values.speakerTitle : 'Speaker Name'}</h3></td></tr>
         <tr><td width="100%"><div id="${
           values?.speakerLibrary?.selected?.id
         }-speakerAbout" class="description" style="text-align: left;padding: 0 16px; margin: 0 0 12px 0; color: ${
