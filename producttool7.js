64313ec1699e8c93475264913c67fce243fbb4d6
sagar-kumar-mz <98985336+sagar-kumar-mz@users.noreply.github.com>
2022-04-08 09:35:46 +0530

    Update producttool7.js

--- producttool7.js
+++ producttool7.js
@@ -1,4 +1,5 @@
 const editorTemplate = `<button id="addProduct" class="button" style="color: ${theme.secondary};background-color:${theme.primary};">Add Speaker</button>`;
+const searchButton =  `<button id="search-btn" class="button" style="width: 20%;color: ${theme.secondary};background-color:${theme.primary};">Search</button>`;
 const productItemsTemplate = _.template(`
 <% _.forEach(products, function(item) { %>
   <div class="product-item" id="product-item" data-uuid='<%= item.id %>' data-title="<%= item.name %>" data-designation="<%= item.designation %>" data-image="<%= item.profile_img %>" data-company="<%= item.company %>" >
@@ -21,7 +22,7 @@ const modalTemplate = function (data) {
         <div class="modal-body">
           <div class="search-box">
             <input type="text" class="form-control" placeholder="Search by speaker name" id="search-bar" style="width: 78%" />
-            <button id="search-btn" class="button" style="width: 20%;color: `${theme.secondary}`;background-color:`${theme.primary}`;">Search</button>
+            ${searchButton}
           </div>
           <div class="products-list">
             ${productItemsTemplate(data)}
