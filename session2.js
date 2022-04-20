1ad9fb6a1af4aadc332743a65eecceefe581bf69
sagar-kumar-mz <98985336+sagar-kumar-mz@users.noreply.github.com>
2022-04-20 10:41:05 +0530

    Update session2.js

--- session2.js
+++ session2.js
@@ -1,7 +1,7 @@
 const editorTemplate = `<button id="session" class="button">Add Session</button>`;
 const searchButton = `<button id="search-btn" class="button">Search</button>`;
 const defaultSpeaker = `
-          <>
+          <div>
             <div class="speaker">
               <img src="https://picsum.photos/100" alt="pic" />
             </div>
@@ -19,10 +19,10 @@ const defaultSpeaker = `
               style="background-color:${theme.accent};color:${theme.secondary};">
               +2
             </div>
-          </>`;
+          </div>`;
 
 const defaultBooth = `
-          <>
+          <div>
             <div class="booth"> 
             <img src="https://picsum.photos/100" alt="pic" />
             </div>
@@ -32,7 +32,7 @@ const defaultBooth = `
             <div class="booth-more" style="background-color:${theme.accent};color:${theme.secondary};"> 
               +2
             </div> 
-          </>`;
+          </div>`;
 
 const speakerAndBoothList = function (values, isPreview) {
   if (values?.speakers?.length || values?.booths?.length || !values?.sessionLibrary?.selected?.id) {
